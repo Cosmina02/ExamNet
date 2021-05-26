3c493f97b056cb406e6d53a85a631027251eeb42
Iordan Cosmina <58854421+Cosmina02@users.noreply.github.com>
2021-05-26 13:04:28 +0300

    some changes

--- ProiectPA/src/main/java/com/example/proiectpa/DBInteraction/Queries.java
+++ ProiectPA/src/main/java/com/example/proiectpa/DBInteraction/Queries.java
@@ -10,13 +10,19 @@ public class Queries {
         String sql= "select * from intrebari where id='" + Qnumber +"'";
         Statement Query= dbConnection.getConn().createStatement();
         ResultSet result= Query.executeQuery(sql);
-        System.out.println(result.getString("TEXT_INTREBARE"));
         return result;
     }
 
     public ResultSet GetAnswers(DBConnection dbConnection,String Qnumber) throws SQLException {
 
         String sql= "select * from RASPUNSURI where q_id='" + Qnumber +"'";
+        Statement Query= dbConnection.getConn().createStatement();
+        ResultSet result= Query.executeQuery(sql);
+        return result;
+
+    }
+    public ResultSet GetProblem(DBConnection dbConnection,String Pnumber) throws SQLException {
+        String sql= "select * from PROBLEME where id='" + Pnumber +"'";
         Statement Query= dbConnection.getConn().createStatement();
         ResultSet result= Query.executeQuery(sql);
         return result;
--- ProiectPA/src/main/java/com/example/proiectpa/controllers/TestController.java
+++ ProiectPA/src/main/java/com/example/proiectpa/controllers/TestController.java
@@ -28,13 +28,12 @@ public class TestController {
     }
 
 @RequestMapping(value="",method=RequestMethod.POST)
-    public ModelAndView processNrOfQuestionsForm(@RequestParam String NrOfQuestions, ModelAndView model) throws SQLException {
-        nrOfQuestions=Integer.parseInt(NrOfQuestions);
-
+    public ModelAndView processNrOfQuestionsForm( ModelAndView model) throws SQLException {
+       // nrOfQuestions=Integer.parseInt(NrOfQuestions);
         Random rand=new Random();
          model.setViewName("/generate/test");
-        for(int i=1;i<=nrOfQuestions;i++){
-            int r=rand.nextInt(10);
+        for(int i=1;i<=10;i++){
+            int r=rand.nextInt(23);
             if(r==0){
                 r++;
             }
@@ -56,6 +55,21 @@ public class TestController {
 
             model.addObject("questions"+i,questions);
        }
+        for(int i=1;i<=2;i++){
+            int r=rand.nextInt(6);
+            if(r==0){
+                r++;
+            }
+            String text = null;
+            ResultSet problema=query.GetProblem(dbconn,"P"+r);
+            if(problema.next()){
+                text=problema.getString("TEXT_PROBLEMA");
+                String dom=problema.getString("DOMENIU");
+                String id=problema.getString("ID");
+
+            }
+            model.addObject("problema"+i,text);
+        }
 
        // System.out.println(nrOfQuestions);
         return model;
