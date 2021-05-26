4fca563f2b9b0614af6849dcfa98be8cf20cf5f0
Iordan Cosmina <58854421+Cosmina02@users.noreply.github.com>
2021-05-26 18:30:38 +0300

    aproape gata

--- ProiectPA/src/main/java/com/example/proiectpa/controllers/TestController.java
+++ ProiectPA/src/main/java/com/example/proiectpa/controllers/TestController.java
@@ -1,13 +1,13 @@
 package com.example.proiectpa.controllers;
 
+import TestModel.Answears;
+import TestModel.Problems;
+import TestModel.Questions;
 import com.example.proiectpa.DBInteraction.DBConnection;
 import com.example.proiectpa.DBInteraction.Queries;
 import com.example.proiectpa.xmlgenerator.CreateXMLFile;
-import org.jdom.Element;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,19 +22,21 @@ import java.util.Random;
 @Controller
 @RequestMapping("/generate")
 public class TestController {
- ArrayList<Questions> QuestionsInTest=new ArrayList<>();
-    DBConnection dbconn=DBConnection.getInstance("./DataBase/database.db");
-    Queries query=new Queries();
-    @RequestMapping(value="",method= RequestMethod.GET)
-    public String index(Model model){
-        model.addAttribute("title","Nr of test Questions");
+    ArrayList<Questions> QuestionsInTest = new ArrayList<>();
+    DBConnection dbconn = DBConnection.getInstance("./DataBase/database.db");
+    Queries query = new Queries();
+    List<Integer> questionNumber = new ArrayList<>();
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("title", "Nr of test Questions");
         return "generate/index";
     }
 
-@RequestMapping(value="",method=RequestMethod.POST)
-    public String processNrOfQuestionsForm( ModelAndView model) throws SQLException {
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String processNrOfQuestionsForm(ModelAndView model) throws SQLException {
 
-    return "redirect:/generate/test";
+        return "redirect:/generate/test";
     }
 
 
@@ -70,38 +72,64 @@ public class TestController {
 
             model.addObject("questions"+i,questions);
         }
+        ArrayList<Problems> problems=new ArrayList<>();
         for(int i=1;i<=2;i++){
             int r=rand.nextInt(6);
             if(r==0){
                 r++;
             }
             String text = null;
+            Problems problem=new Problems();
             ResultSet problema=query.GetProblem(dbconn,"P"+r);
             if(problema.next()){
                 text=problema.getString("TEXT_PROBLEMA");
+                problem.setText_problema(text);
                 String dom=problema.getString("DOMENIU");
-                String id=problema.getString("ID");
+                problem.id=problema.getString("ID");
 
             }
+            problems.add(problem);
             model.addObject("problema"+i,text);
         }
-
+        Questions quest=new Questions();
+        quest.setId("Problem");
+        quest.setProblems(problems);
+        QuestionsInTest.add(quest);
         // System.out.println(nrOfQuestions);
         CreateXMLFile.generateTestXML(QuestionsInTest);
         System.out.println(QuestionsInTest);
         return model;
     }
 
-    @RequestMapping(value="/test",method = RequestMethod.POST)
-    public String processAnswears(@RequestParam List<String> question){
-        List<String> answears=question;
+
+
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public String processAnswears(@RequestParam List<String> question) {
+        List<String> answears = question;
+        CreateXMLFile.generateAnswearsXML(QuestionsInTest, answears);
         System.out.println(answears);
-       return "redirect:/generate/succes";
+        return "redirect:/generate/succes";
     }
 
-    @RequestMapping(value="/succes")
-    public String getSucces(){
+    @RequestMapping(value = "/succes")
+    public String getSucces() {
         return "generate/succes";
+    }
+
+    private boolean check_rand_number(int nr) {
+
+        if (nr == 0) {
+            nr++;
+        }
+        if (questionNumber.isEmpty()) {
+            questionNumber.add(nr);
+            return false;
+        } else {
+            if (questionNumber.contains(nr))
+                return true;
+            else
+                return false;
+        }
     }
 
 }
